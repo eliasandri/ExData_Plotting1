9f1242f53c16a7186ea318a38e90bcc53e9c9e03
elias <eliasandriasgeirsson@gmail.com>
2020-01-18 16:43:56 +0000

    Forgot the variable fileName

--- plot4.R
+++ plot4.R
@@ -1,3 +1,4 @@
+fileName <- "household_power_consumption.txt"
 data <- read.table(fileName, skip = 1, sep = ";", na.strings = "?")
 
 names(data) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
